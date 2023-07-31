5ea8be35eb9f2b45b59d4d873b189f8ffd4bba79
Vladyslav Morschch <vmorshch8@gmail.com>
2023-07-31 14:18:17 +0300

    fix dev plugins

--- config/webpack/build/buildPlugins.ts
+++ config/webpack/build/buildPlugins.ts
@@ -6,9 +6,10 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-	return [
+	const plugins = [
 		new HtmlWebpackPlugin({template: paths.html}),
-		new CleanWebpackPlugin(),
+		new
+		CleanWebpackPlugin(),
 		new ProgressPlugin(),
 		new MiniCssExtractPlugin({
 			filename: 'css/[name].[contenthash:8].css',
@@ -17,9 +18,13 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
 		new webpack.DefinePlugin({
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
-		new webpack.HotModuleReplacementPlugin(),
-		new BundleAnalyzerPlugin({
-			openAnalyzer:false
-		})
+
 	]
+	if (isDev) {
+		plugins.push(new webpack.HotModuleReplacementPlugin())
+		plugins.push(new BundleAnalyzerPlugin({
+			openAnalyzer: false
+		}))
+	}
+	return plugins
 }
